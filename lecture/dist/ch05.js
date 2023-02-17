0b2ba6a6d1d760c0e51a6841c4de531e29335a45
devKya <kihaok@gmail.com>
2023-02-18 02:05:25 +0900

    delete dist

--- lecture/dist/ch05.js
+++
@@ -1,5 +0,0 @@
-var tiger = '52';
-var soya2 = {
-    name: 'soya'
-};
-var position = { x: 10, y: 20 };
--- lecture/dist/ch06.js
+++
@@ -1,9 +0,0 @@
-// Literal Types은 const 의 업글버전임
-function gameFunc(player) {
-    return [player];
-}
-var 자료 = {
-    name: 'Jung'
-};
-function myFunc3(a) {
-}
